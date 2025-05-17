f5b8f700e481c56ab3e37ed601a43515b19f1feb
PraveenBushipaka <praveenbushipaka942@gmail.com>
2025-05-17 10:16:42 +0200

    Create data_module.py

---
+++ data_module.py
@@ -0,0 +1,113 @@
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+from transformers import PreTrainedTokenizer, default_data_collator
+from typing import Tuple
+import math
+import pandas as pd
+from typing import Dict, List, Set, Tuple, Any
+import itertools
+import random
+
+
+# this function takes a tokenizer, max_length: int, question: str, answer: str. Returns their input_ids, labels, attention_mask.
+# Internally it also applies tokenizer.apply_chat_template, which is only used for chat based or instruct based models. 
+# the code also pads tokens to max_length.
+
+def convert_raw_data_to_model_qa(tokenizer, max_length,  question, answer):
+    question = str(question)
+    answer = str(answer)
+    
+    messages = [{"role": "user", "content": question}]
+    new_question = tokenizer.apply_chat_template(
+        messages,
+        tokenizer = False,
+        add_generataion_prompt=True
+    )
+    
+    full_text = str(new_question) + answer
+    num_question_tokens = len(tokenizer.tokenize(str(new_question), add_special_tokens=True))
+
+    encoded = tokenizer(
+        full_text, 
+        add_special_tokens=True, 
+        max_length=max_length, 
+        truncation=True, 
+    )
+    pad_length = max_length - len(encoded.input_ids)
+    
+    pad_input_ids = encoded['input_ids'] + [tokenizer.eos_token_id] * pad_length
+    pad_attention_mask = encoded['attention_mask'] + [0] * pad_length
+    if len(encoded.input_ids) == max_length:
+        label = encoded.input_ids
+    else:
+        label = encoded['input_ids'] + [tokenizer.eos_token_id] + [-100] * (pad_length-1)
+
+    #change label to -100 for question tokens
+    for i in range(num_question_tokens): label[i] = -100
+
+    return torch.tensor(pad_input_ids),torch.tensor(label),torch.tensor(pad_attention_mask)
+
+
+
+# this function creates a pytorch dataset for the given dataframe or csv path. Return a tuple of input_ids, labels, attention_mask
+
+class SingleDataset(Dataset):
+    def __init__(self, data_path, 
+                 tokenizer, 
+                 max_length=512, 
+                 template_format=None, 
+                 question_key = 'question',
+                 answer_key = 'answer'):
+        """
+        Initializes the dataset for gradient ascent finetuning
+        
+        Args:
+            data_path (str): path to the data file. csv file containing columns 'question' and 'answer'
+            tokenizer (transformers.PreTrainedTokenizer): tokenizer to process the input
+            max_length (int, optional): maximum sequence length for tokenization. Defaults to 512.
+            template_format (str, optional): format template for structuring input
+        """
+        self.data = pd.read_csv(data_path)
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.template_format = template_format
+        self.qk = question_key
+        self.ak = answer_key
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        question = self.data.iloc[idx][self.qk]
+        answer = self.data.iloc[idx][self.ak]
+        return convert_raw_data_to_model_qa(
+            tokenizer=self.tokenizer, 
+            max_length=self.max_length, 
+            question=question, 
+            answer=answer,
+            template_format=self.template_format
+        )
+
+
+
+
+
+
+
+
+def custom_data_collator_forget(samples):
+    """
+    Collate function for the forget dataset only
+
+    Args:
+        samples (list of tuples): Each tuple contains (input_ids, labels, attention_mask)
+
+    Returns:
+        dict: batched_inputs, labels, attention_masks.
+
+    """
+    input_ids = torch.stack([sample[0] for sample in samples])
+    labels = torch.stack([sample[1] for sample in samples])
+    attention_mask = torch.stack([sample[2] for sample in samples])
+    return {'input_ids': input_ids, 'labels': labels, 'attention_mask': attention_mask}
