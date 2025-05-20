4b4ca765c9f1a979adf1dc8ccbd75364f40730ac
PraveenBushipaka <praveenbushipaka942@gmail.com>
2025-05-20 12:23:11 +0200

    Create collators.py

---
+++ collators.py
@@ -0,0 +1,34 @@
+def custom_gd_collator_forget(samples): # for vanilla/cyclic gradient difference, also can be extended to dpo, npo type
+    """
+    Custom data collator for forget and retain data
+
+    Args:
+        samples: list of tuples (forget_data, retain_data) from the DualDataset class
+
+    Returns:
+        rets: list of tuples (input_ids, labels, attention_mask)
+        example output for batch size 2
+        
+        [(  #forget data for batch of 2
+            torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]]), # input_ids
+            torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]]), # labels
+            torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]]), # attention_mask
+            ),
+            (  #retain data for batch of 2
+            torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]]), # input_ids
+            torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]]), # labels
+            torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]]), # attention_mask
+            ),
+        ]
+
+    """
+
+    forget_samples, retain_samples = [sample[0] for sample in samples], [sample[1] for sample in samples]
+    rets = []
+    for data_type in ["forget", "retain"]:
+        data = forget_samples if data_type == "forget" else retain_samples
+        input_ids = [s[0] for s in data]
+        labels = [s[1] for s in data]
+        attention_mask = [s[2] for s in data]
+        rets.append((torch.stack(input_ids), torch.stack(labels), torch.stack(attention_mask)))
+    return rets
