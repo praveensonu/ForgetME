c0fb53a0afe47f68af1899ef19554d89e2a803e7
PraveenBushipaka <praveenbushipaka942@gmail.com>
2025-05-20 12:26:02 +0200

    Update forget_loss.py
    added vanilla grad diff trainer

--- forget_loss.py
+++ forget_loss.py
@@ -48,3 +48,25 @@ class GATrainer(Trainer):
 
         loss = forget_loss
         return (loss, outputs) if return_outputs else loss
+
+
+
+class GradDiffTrainer(Trainer):
+    
+    def compute_loss(self, model, inputs, return_outputs = False, num_items_in_batch = None):
+        forget_inputs, retain_inputs = inputs
+        input_ids, labels, attention_mask = forget_inputs
+
+        ## gradient ascent on the forget
+        outputs = model(input_ids,labels=labels, attention_mask=attention_mask)
+        forget_loss = outputs.loss
+        forget_loss = forget_loss * -1
+
+        ## gradient descent on the retain
+        retain_input_ids, retain_labels, retain_attention_mask = retain_inputs
+        retain_outputs = model(retain_input_ids,labels=retain_labels, attention_mask=retain_attention_mask)
+        retain_loss = retain_outputs.loss
+        loss = forget_loss + retain_loss
+
+        return (loss, outputs) if return_outputs else loss
+    
