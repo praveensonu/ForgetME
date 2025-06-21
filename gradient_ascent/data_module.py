3ce2becbe09502907e6b575ae341b3a915b5299e
PraveenBushipaka <praveenbushipaka942@gmail.com>
2025-06-21 12:28:09 +0200

    Update data_module.py

--- gradient_ascent/data_module.py
+++ gradient_ascent/data_module.py
@@ -1,1 +1,66 @@
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+from transformers import PreTrainedTokenizer, default_data_collator
+from typing import Tuple
+import math
+import pandas as pd
+from typing import Dict, List, Set, Tuple, Any
 
+
+def convert_raw_data_to_model_qa(tokenizer, max_length,  question, answer):
+    question = str(question)
+    answer = str(answer)
+    full_text = question + answer
+    num_question_tokens = len(tokenizer.tokenize(question, add_special_tokens=False)) #this is important, we 
+    encoded = tokenizer(
+        full_text,
+        add_special_tokens=False, #this is important, we keep false cause we already added the special tokens from template
+        max_length=max_length,
+        truncation=True,
+    )
+    pad_length = max_length - len(encoded.input_ids)
+    pad_input_ids = encoded['input_ids'] + [tokenizer.eos_token_id] * pad_length
+    pad_attention_mask = encoded['attention_mask'] + [0] * pad_length
+    if len(encoded.input_ids) == max_length:
+        label = encoded.input_ids
+    else:
+        label = encoded['input_ids'] + [tokenizer.eos_token_id] + [-100] * (pad_length-1)
+    #change label to -100 for question tokens, including assistant header and end of header.
+    for i in range(num_question_tokens): label[i] = -100
+    return torch.tensor(pad_input_ids),torch.tensor(label),torch.tensor(pad_attention_mask)
+
+
+class SingleDataset(Dataset):
+    def __init__(self, forget_data,
+                 tokenizer,
+                 max_length=512,
+                 question_key = 'question',
+                 answer_key = 'answer'):
+        """
+        Initializes the dataset for gradient ascent finetuning
+
+        Args:
+            data_path (str): path to the data file. csv file containing columns 'question' and 'answer'
+            tokenizer (transformers.PreTrainedTokenizer): tokenizer to process the input
+            max_length (int, optional): maximum sequence length for tokenization. Defaults to 512.
+            template_format (str, optional): format template for structuring input
+        """
+        self.data = forget_data.reset_index(drop=True)
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.qk = question_key
+        self.ak = answer_key
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        question = self.data.iloc[idx][self.qk]
+        answer = self.data.iloc[idx][self.ak]
+        return convert_raw_data_to_model_qa(
+            tokenizer=self.tokenizer,
+            max_length=self.max_length,
+            question=question,
+            answer=answer
+        )
