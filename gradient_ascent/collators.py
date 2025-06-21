d4dc511a0456a00609070fb97ff5be49bbb236d3
PraveenBushipaka <praveenbushipaka942@gmail.com>
2025-06-21 12:27:30 +0200

    Update collators.py

--- gradient_ascent/collators.py
+++ gradient_ascent/collators.py
@@ -1,1 +1,20 @@
+import torch
+from transformers import  default_data_collator
+from typing import Dict, List, Tuple 
 
+
+def custom_data_collator_forget(samples): # for vanilla gradient ascent
+    """
+    Collate function for the forget dataset only
+
+    Args:
+        samples (list of tuples): Each tuple contains (input_ids, labels, attention_mask)
+
+    Returns:
+        dict: batched_inputs, labels, attention_masks.
+
+    """
+    input_ids = torch.stack([sample[0] for sample in samples])
+    labels = torch.stack([sample[1] for sample in samples])
+    attention_mask = torch.stack([sample[2] for sample in samples])
+    return {'input_ids': input_ids, 'labels': labels, 'attention_mask': attention_mask}
