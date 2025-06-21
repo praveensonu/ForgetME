f18a6f3ecf02a19f975bb836d5308452055c5ffd
PraveenBushipaka <praveenbushipaka942@gmail.com>
2025-06-21 12:28:33 +0200

    Update gradient_ascent.py

--- gradient_ascent/gradient_ascent.py
+++ gradient_ascent/gradient_ascent.py
@@ -1,1 +1,11 @@
-
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments
+from config import Config
+from peft import  LoraConfig, get_peft_model
+from data_module import DualDataset, SingleDataset, DualTitleDataset, DualDatasetRandom
+from collators import custom_gd_collator_forget, custom_data_collator_forget
+from utils import find_all_linear_names
+from forget_trainer import GATrainer, GradDiffTrainer
+from accelerate import Accelerator
+import pandas as pd
+from template import LLAMA3_CHAT_TEMPLATE
