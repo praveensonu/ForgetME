2021fe2dbed6ef9450fc51183f72df0c402d8be9
PraveenBushipaka <praveenbushipaka942@gmail.com>
2025-06-21 12:26:48 +0200

    Update trainer.py

--- gradient_ascent/trainer.py
+++ gradient_ascent/trainer.py
@@ -1,1 +1,28 @@
+from transformers import Trainer
+import torch.nn as nn
+from accelerate import Accelerator
+
+accelerator = Accelerator()
+
+class GATrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch = None):
+        """
+        Computes the gradient ascent loss for the model
+        """
+        #if self.loss_type == 'grad_ascent':
+        # unpack the forget inputs
+        input_ids = inputs['input_ids']
+        labels = inputs['labels']
+        attention_mask = inputs['attention_mask']
+
+        # forward pass
+        outputs = model(
+            input_ids = input_ids,
+            attention_mask = attention_mask,
+            labels = labels
+        )
+        forget_loss = outputs.loss * -1 # gradient ascent is negating the loss
+
+        loss = forget_loss
+        return (loss, outputs) if return_outputs else loss
 
